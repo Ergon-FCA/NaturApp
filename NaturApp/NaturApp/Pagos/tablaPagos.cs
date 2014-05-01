f7d75ed92793bb46210b3299d4cbbeeb4b76251d
Ergon-FCA <ergon_fca@outlook.es>
2014-05-01 13:54:19 -0500

    Pagos y pedidos inicial

---
+++ NaturApp/NaturApp/Pagos/tablaPagos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SQLite;
+
+namespace NaturApp.Pagos
+{
+    public class tablaPagos
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idPago { get; set; }
+        public int idCliente { get; set; }
+        public int idPedido { get; set; }
+        public double cantidadPago { get; set; }
+        public DateTime fechaPago { get; set; }
+
+    }
+}
---
+++ NaturApp/NaturApp/Pedidos/tablaPedidos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SQLite;
+
+namespace NaturApp.Pedidos
+{
+    public class tablaPedidos
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idPedido { get; set; }
+        public int idCliente { get; set; }
+        public string producto { get; set; }
+        public double precio { get; set; }
+        public int cantidad { get; set; }
+        public DateTime fechaPedido { get; set; }
+        public double totalPedido { get; set; }
+    }
+}
